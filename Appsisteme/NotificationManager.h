86ebc9f5c7fe05ac9d9bf5a76cc4aee468d3372e
DevelopersOfTalent <appsisteme.team@gmail.com>
2016-05-20 10:07:19 +0200

    Añadido Notification Manager

---
+++ Appsisteme/NotificationManager.h
@@ -0,0 +1,13 @@
+//
+//  NotificationManager.h
+//  Appsisteme
+//
+//  Created by Alumno on 20/05/16.
+//  Copyright © 2016 Alumno. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NotificationManager : NSObject
+
+@end
---
+++ Appsisteme/NotificationManager.m
@@ -0,0 +1,13 @@
+//
+//  NotificationManager.m
+//  Appsisteme
+//
+//  Created by Alumno on 20/05/16.
+//  Copyright © 2016 Alumno. All rights reserved.
+//
+
+#import "NotificationManager.h"
+
+@implementation NotificationManager
+
+@end
