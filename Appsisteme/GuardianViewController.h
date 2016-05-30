f89813ecf98eb3315c95bd93eab5bfd83cb44b51
DevelpersOfTalent <appsisteme.team@gmail.com>
2016-05-30 10:17:53 +0200

    GuardianNavigationController rehecho

---
+++ Appsisteme/GuardianViewController.h
@@ -0,0 +1,13 @@
+//
+//  GuardianViewController.h
+//  Appsisteme
+//
+//  Created by Andrea Cerrato on 30/5/16.
+//  Copyright © 2016 Alumno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GuardianViewController : UINavigationController
+
+@end
---
+++ Appsisteme/GuardianViewController.m
@@ -0,0 +1,24 @@
+//
+//  GuardianViewController.m
+//  Appsisteme
+//
+//  Created by Andrea Cerrato on 30/5/16.
+//  Copyright © 2016 Alumno. All rights reserved.
+//
+
+#import "GuardianViewController.h"
+
+@interface GuardianViewController ()
+
+@end
+
+@implementation GuardianViewController
+    
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+
+    [[UINavigationBar appearance] setBackgroundImage:[UIImage imageNamed:@"cabecera_vigilado_260.jpg"] forBarMetrics:UIBarMetricsDefault];
+}
+
+@end
